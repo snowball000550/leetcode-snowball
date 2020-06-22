f23175b6afb22c91c3e4a8a96c81bc7a03c66079
snowball <13681115889>
2020-06-22 18:33:25 +0800

    fix markdown

--- parseMD.js
+++ parseMD.js
@@ -8,6 +8,8 @@ const data6 = ['#a','# #b','## #c','#d','## e','###f','# ##g', '##h','##i', '###
 const data7 = []
 const data8 = ['#a','b','## #c']
 const data9 = ['#a','##b','###c','#d','###e',]
+const data10 = ['a']
+
 
 
 parseMD(data)
@@ -15,53 +17,80 @@ parseMD(data)
 var parseMD = function(data){
     const orderArr = [];
     const newArr = [];
-    if (data.length === 0) {
-        return [];
-    }
-    if (data.length === 1 && data[0].split('#').length === 2) {
-        return [1];
-    }
-    for(let i = 0; i < data.length; i++){
-        let order = '';
-        let index = data[i].split('#').length - 1;
-        if ((i >= 1 && data[i].split('#').length - data[i-1].split('#').length > 1) || data[i].split('#').length === 0) {
-            return 'error'
+
+    try {
+        if (data.length === 0) {
+            return [];
         }
-        if ( i === 0 || !orderArr[index-1] || data[i].split('#').length - data[i-1].split('#').length === 1 ) {
-            orderArr[index-1] = 1;
-        } else {
-            orderArr[index-1] += 1;
+        if (data.length === 1 && data[0].split('#').length === 2) {
+            return [1];
+        } else if (data[0].split('#').length === 1){
+            throw '格式错误';
         }
-        for (let i = 0; i < index; i++) {
-            order += index ===1 || i === index - 1 ? orderArr[i] : orderArr[i] + '.';
-        }
-        newArr.push(order)
+        for(let i = 0; i < data.length; i++){
+                let order = '';
+                let index = data[i].split('#').length - 1;
+                if ((i >= 1 && data[i].split('#').length - data[i-1].split('#').length > 1) || data[i].split('#').length === 0) {
+                    throw '格式错误';
+                }
+                if ( i === 0 || !orderArr[index-1] || data[i].split('#').length - data[i-1].split('#').length === 1 ) {
+                    orderArr[index-1] = 1;
+                } else {
+                    orderArr[index-1] += 1;
+                }
+                for (let i = 0; i < index; i++) {
+                    order += index ===1 || i === index - 1 ? orderArr[i] : orderArr[i] + '.';
+                }
+                newArr.push(order)
+            }
+            return newArr;
     }
-    return newArr;
+    catch(err){
+        alert(err);
+        return;
+    }   
 }
 
 // with title
 var parseMD = function(data) {
     const orderArr = [];
     const newArr = [];
-    for(let i = 0; i < data.length; i++){
-        let order = '';
-        let index = data[i].split('#').length - 1;
-        if ((i >= 1 && data[i].split('#').length - data[i-1].split('#').length > 1) || data[i].split('#').length === 0) {
-            return 'error'
+    
+    try {
+        if (data.length === 0) {
+            return [];
         }
-        if ( i === 0 || !orderArr[index-1] || data[i].split('#').length - data[i-1].split('#').length === 1 ) {
-            orderArr[index-1] = 1;
-        } else {
-            orderArr[index-1] += 1;
+        if (data.length === 1 && data[0].split('#').length === 2) {
+            return [{
+                title: data[0].split('#').pop(),
+                order: 1
+            }];
+        } else if (data[0].split('#').length === 1){
+            throw '格式错误';
         }
-        for (let i = 0; i < index; i++) {
-            order += index ===1 || i === index - 1 ? orderArr[i] : orderArr[i] + '.';
-        }
-        newArr.push({
-            title: data[i].split('#').pop(),
-            order
-        })
+        for(let i = 0; i < data.length; i++){
+                let order = '';
+                let index = data[i].split('#').length - 1;
+                if ((i >= 1 && data[i].split('#').length - data[i-1].split('#').length > 1) || data[i].split('#').length === 0) {
+                    throw '格式错误';
+                }
+                if ( i === 0 || !orderArr[index-1] || data[i].split('#').length - data[i-1].split('#').length === 1 ) {
+                    orderArr[index-1] = 1;
+                } else {
+                    orderArr[index-1] += 1;
+                }
+                for (let i = 0; i < index; i++) {
+                    order += index ===1 || i === index - 1 ? orderArr[i] : orderArr[i] + '.';
+                }
+                newArr.push({
+                    title: data[i].split('#').pop(),
+                    order
+                })
+            }
+            return newArr;
     }
-    return newArr;
+    catch(err){
+        alert(err);
+        return;
+    }
 }
